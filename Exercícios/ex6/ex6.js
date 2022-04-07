da1fa26320930b2bc061e56906ffb75e4fcff9b5
Alex-Hugo <100318918+Alex-Hugo@users.noreply.github.com>
2022-04-07 17:14:43 -0300

    Create ex6.js

---
+++ Exercícios/ex6/ex6.js
@@ -0,0 +1,6 @@
+let nome=document.getElementById("nome")
+let sobrenome=document.getElementById("sobrenome")
+const botão = document.getElementById("botão");
+function botao(){
+alert("Olá"+document.getElementById("nome").value+document.getElementById("sobrenome").value);
+}
